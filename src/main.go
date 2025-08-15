86cbb7f0097a2babe9f3a960097add2233220563
Christian Hieke <christian.hieke@googlemail.com>
2025-08-15 22:30:23 +0200

    refactor: Move draw functions to own file

--- src/main.go
+++ src/main.go
@@ -144,25 +144,3 @@ func loadConfig() Config {
 
 	return config
 }
-
-func drawStatusbar(screen tcell.Screen) {
-	statusbarQuitLabel := "Quit"
-	statusbarQuitShortcut := "Q, <C-c>"
-	width, height := screen.Size()
-
-	x := (width - len(statusbarQuitLabel+statusbarQuitShortcut) + 1) / 2
-	for i, r := range statusbarQuitLabel {
-		screen.SetContent(x+i, height-1, r, nil, tcell.StyleDefault.Bold(true))
-	}
-	for i, r := range " " + statusbarQuitShortcut {
-		screen.SetContent(x+4+i, height-1, r, nil, tcell.StyleDefault)
-	}
-}
-
-func drawTextCentered(s tcell.Screen, y int, text string, style tcell.Style) {
-	w, _ := s.Size()
-	x := (w - len(text)) / 2
-	for i, r := range text {
-		s.SetContent(x+i, y, r, nil, style)
-	}
-}
